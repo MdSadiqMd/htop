aff1d9a67465f28f44dbcddeb350e0385ed830e2
Md.Sadiq <mohammadsadiq4950@gmail.com>
2025-09-25 15:24:17 +0530

    cleanup and compatable with multiple clients

--- src/main.rs
+++ src/main.rs
@@ -9,22 +9,37 @@ use std::sync::{Arc, Mutex};
 use sysinfo::System;
 use tokio::net::TcpListener;
 
-#[derive(Clone)]
+#[derive(Default, Clone)]
 struct AppState {
-    sys: Arc<Mutex<System>>,
+    cpus: Arc<Mutex<Vec<f32>>>,
 }
 
 #[tokio::main]
 async fn main() {
+    let app_state = AppState::default();
+    let app_state_for_bg = app_state.clone();
+
     let app = Router::new()
         .route("/healthz", get(|| async { "up" }))
         .route("/", get(root_get))
         .route("/index.mjs", get(indexmjs_get))
         .route("/index.css", get(indexcss_get))
         .route("/htop", get(sys_info))
-        .with_state(AppState {
-            sys: Arc::new(Mutex::new(System::new())),
-        });
+        .with_state(app_state.clone());
+
+    tokio::task::spawn_blocking(move || {
+        let mut sys = System::new();
+        loop {
+            sys.refresh_cpu_usage();
+            let v: Vec<_> = sys.cpus().iter().map(|cpu| cpu.cpu_usage()).collect();
+            {
+                let mut cpus = app_state_for_bg.cpus.lock().unwrap();
+                *cpus = v;
+            }
+            std::thread::sleep(std::time::Duration::from_millis(250));
+        }
+    });
+
     let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
@@ -53,10 +68,8 @@ async fn indexcss_get() -> impl IntoResponse {
         .unwrap()
 }
 
+#[axum::debug_handler]
 async fn sys_info(State(state): State<AppState>) -> impl IntoResponse {
-    let mut sys = state.sys.lock().unwrap();
-    sys.refresh_cpu_all();
-
-    let v: Vec<_> = sys.cpus().iter().map(|cpu| cpu.cpu_usage()).collect();
+    let v = state.cpus.lock().unwrap().clone();
     Json(v)
 }
